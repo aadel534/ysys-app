09200053346dccc4591837480b09ceba66ca8d95
aadel534 <56454954+aadel534@users.noreply.github.com>
2021-03-02 13:08:23 +0000

    Create characterscardupdate2.js

---
+++ src/components/STRETCH_Cards/characterscardupdate2.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from 'react';
+
+function CharactersCard() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        getCharacters();
+      }, []);
+    const getCharacters= async () => {
+        const response = await fetch("https://anapioficeandfire.com/api/characters/");
+        const data = await response.json();
+        console.log(data)
+        setCharacters(data);
+
+      };
+
+ 
+
+
+
+    return (
+<div>       
+
+{characters.map(character => (
+        <h1>{character.name}</h1>
+      ))}
+      ;
+      </div>
+    );
+  }
+  
+  export default CharactersCard;
+  
