1e6347abceaee7a6180113955b57d54b8e186bb8
aadel534 <56454954+aadel534@users.noreply.github.com>
2021-03-02 13:07:50 +0000

    Create housecardupdate2.js

---
+++ src/components/STRETCH_Cards/housecardupdate2.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react';
+
+function HouseCard() {
+    const [house, setHouse] = useState([]);
+
+    useEffect(() => {
+        getHouse();
+      }, []);
+
+      const getHouse =async () => {
+        const response = await fetch("https://anapioficeandfire.com/api/houses/");
+        const data = await response.json();
+        console.log(data)
+        setHouse(data);
+
+      };
+
+
+
+
+    return (
+      <div >
+       {house.map(house=> (
+        <h1>{house.name}</h1>
+      ))}
+      ;
+      </div>
+    );
+  }
+  
+  export default HouseCard;
+  
